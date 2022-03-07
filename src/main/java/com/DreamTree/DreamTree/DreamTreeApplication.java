92c2c67210346fec6d249104ccc9438bccf519db
panghoon <leegwanghoon95@gmail.com>
2022-03-07 14:06:46 +0900

    add moa

--- src/main/java/com/DreamTree/DreamTree/DreamTreeApplication.java
+++ src/main/java/com/DreamTree/DreamTree/DreamTreeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DreamTreeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DreamTreeApplication.class, args);
 	}
-
 }
